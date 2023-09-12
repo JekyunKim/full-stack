8cc866bd59aab375b4249fa79a0b8c150c8eda49
Jekyun Kim <kkyuni97@naver.com>
2023-09-12 09:22:13 +0900

    viewset permission 권한 수정, auth 앱 추가

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
     'core',
     'core.user',
-    'rest_framework',
+    'core.auth',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core_user.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS':
+    ['django_filters.rest_framework.DjangoFilterBackend'],
+}
+
