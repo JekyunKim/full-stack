9d8bd56f52fad2180edc960ca6da680a952b9692
Jekyun Kim <kkyuni97@naver.com>
2023-09-14 01:29:50 +0900

    comment 앱 추가 까지

--- core/user/models.py
+++ core/user/models.py
@@ -43,6 +43,7 @@ class User(AbstractModel, AbstractBaseUser, PermissionsMixin):
     is_superuser = models.BooleanField(default=False)
     bio = models.TextField(null=True)
     avatar = models.ImageField(null=True)
+    posts_liked = models.ManyToManyField("core_post.Post", related_name='liked_by')
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
     objects = UserManager()
@@ -53,3 +54,12 @@ class User(AbstractModel, AbstractBaseUser, PermissionsMixin):
     @property
     def name(self):
         return f"{self.first_name} {self.last_name}"
+
+    def like(self, post):
+        return self.posts_liked.add(post)
+
+    def remove_like(self, post):
+        return self.posts_liked.remove(post)
+
+    def has_liked(self, post):
+        return self.posts_liked.filter(pk=post.pk).exists()
