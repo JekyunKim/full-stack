38d17778a9ef89ab90f67e06a130c01d18dfa175
Jekyun Kim <kkyuni97@naver.com>
2023-09-19 11:42:04 +0900

    09.19

---
+++ social-media-app/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import Home from "./pages/Home";
+import Login from "./pages/Login"
+import Registration from "./pages/Registration";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={
+        <ProtectedRoute>
+          <Home />
+        </ProtectedRoute>} 
+      />
+      <Route path="/login/" element={<Login/>} />
+      <Route path="/register/" element={<Registration/>} />
+    </Routes>
+  );
+}
+
+export default App;
